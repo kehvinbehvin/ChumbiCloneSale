571b54b091502926bc4390af75eaed329b2194de
Kevin Ho <hokevin78@gmail.com>
2022-02-01 01:08:54 +0800

    feature: add script to interact with Sale contract

--- scripts/interact.js
+++ scripts/interact.js
@@ -3,11 +3,11 @@ require("@nomiclabs/hardhat-ethers");
 
 const NODE_PROVIDER_API_KEY = process.env.NODE_PROVIDER_API_KEY;
 const SIGNER_PRIVATE_KEY = process.env.SIGNER_PRIVATE_KEY;
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CHUMBICLONESALE_CONTRACT_ADDRESS = process.env.CHUMBICLONESALE_CONTRACT_ADDRESS;
 
 // Contract abi
 // To uncomment and insert <CONTRACT NAME>
-const contract = require("../artifacts/contracts/<CONTRACT>.sol/<CONTRACT>.json");
+const contract = require("../artifacts/contracts/ChumbiCloneSale.sol/ChumbiCloneSale.json");
 console.log("Contract abi retrieved")
 
 // Provider
@@ -16,22 +16,21 @@ console.log("Contract abi retrieved")
 const alchemyProvider = new ethers.providers.AlchemyProvider(network="maticmum", NODE_PROVIDER_API_KEY);
 console.log("Alchemy connected")
 
+
 // Signer
 const signer = new ethers.Wallet(SIGNER_PRIVATE_KEY, alchemyProvider);
 console.log("Signer connected to alchemy")
 
 // Contract
-// Uncomment and replace with your own contract
-// const YourContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
-console.log("Sale contract instantiated")
+const ChumbiCloneSale = new ethers.Contract(CHUMBICLONESALE_CONTRACT_ADDRESS, contract.abi,  signer);
+console.log("ChumbiCloneSale contract instantiated")
 
 async function main() {
     try {
-        // Uncomment and replace with your contract
-        // const responseObject = await <YourContract>.method();
-        // console.log(JSON.stringify(responseObject));
-
-    } catch {
+        const responseObject = await ChumbiCloneSale.purchase(1);
+        console.log(JSON.stringify(responseObject));
+    } catch(error) {
+        console.log(error)
         console.log("Error with interacting with contract")
     };
 }
